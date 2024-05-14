616869ccefb0cc76f662df374f22c2111310d248
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-14 07:53:51 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogUdla_PZ_JS_DM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
